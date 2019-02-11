4f677126621c0abf746e3b50c747043aad30fd68
Jainam Shah <meetjainam39@gmail.com>
2019-02-11 19:22:54 +0530

    Update csv-to-json-to-mongo.py

--- csv-to-json-to-mongo.py
+++ csv-to-json-to-mongo.py
@@ -1,19 +1,20 @@
 import csv
 import json
 
-f = open('final.csv', 'rU')
+f = open('filename.csv', 'rU')
 
 reader = csv.DictReader(f, fieldnames=("Feature col 1", "Feature col 2"))
 
 # Parse the CSV into JSON
 out = json.dumps([row for row in reader])
 
-f = open('Finaldataset.json', 'w')
+f = open('Filename.json', 'w')
 
 f.write(out)
 
 print("JSON saved!")
 
+#run this below command in terminal not in mongodb shell
 #to json to csv
 #mongoimport --db dbName --collection collectionName --file parsed.json --jsonArray
 
